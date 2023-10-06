d507c3ce072a1bb5949d178da78f011ffd377ff6
Saharsh365 <67753821+Saharsh365@users.noreply.github.com>
2023-10-05 20:41:19 -0400

    Add files via upload

---
+++ hello/HelloWorld.java
@@ -0,0 +1,12 @@
+/* *****************************************************************************
+ *  Name:              Saharsh Maloo
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Hello, World");
+    }
+}
---
+++ hello/RandomWord.java
@@ -0,0 +1,24 @@
+/* *****************************************************************************
+ *  Name:              Saharsh Maloo
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+    public static void main(String[] args) {
+        String champion = "";
+        int p = 1;
+        while (!StdIn.isEmpty()) {
+            String word = StdIn.readString();
+            if (StdRandom.bernoulli(1.0 / p)) {
+                champion = word;
+            }
+            p++;
+        }
+        StdOut.println(champion);
+    }
+}
